a1ddb0300b518049a6e4734f7a515559dacee3c5
IRONDESK <zerozerobase@gmail.com>
2022-10-27 16:04:27 +0900

    [Refactor] 스타일링 세팅 수정

--- src/styles/PALLETS.ts
+++ src/styles/PALLETS.ts
@@ -2,8 +2,13 @@ const COLOR = {
   background: "#FAFAFA",
   main: "#F28C0F",
   mainrgb: "242,140,15",
-  bull: "#D47073",
-  bear: "#6D84D8",
+  bull: "#FF4E43",
+  bear: "#5078F0",
 }
 
-export default COLOR
+const SHADOW = {
+  basic: "0px 12px 12px rgba(0, 0, 0, 0.1);",
+  center: "0px -4px 12px rgba(0, 0, 0, 0.05)",
+}
+
+export { COLOR, SHADOW }
