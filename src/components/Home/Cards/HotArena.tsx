25c11388e2a2877c88ba5ff42be4bcb0ea15754b
IRONDESK <zerozerobase@gmail.com>
2022-11-04 14:19:17 +0900

    [Refactor] 핫토픽아레나 토론 Mock 데이터 파일 분리

--- src/components/Home/Cards/HotArena.tsx
+++ src/components/Home/Cards/HotArena.tsx
@@ -1,44 +1,25 @@
-import React from "react"
-import styled from "@emotion/styled"
-import DebateCard from "../DebateCard"
-import Link from "next/link"
+import React from "react";
+import styled from "@emotion/styled";
+import Link from "next/link";
 
-const DummyTopic = [
-  {
-    id: 1,
-    title: "삼성전자,\n'10만전자' 가나",
-    finishDate: "2022-10-26",
-    active: true,
-  },
-  {
-    id: 2,
-    title: "3기 신도시, 이번 정부 안에 삽 뜰까?",
-    finishDate: "2022-11-10",
-    active: false,
-  },
-  {
-    id: 3,
-    title: "주식 급락 언제 쯤 진정될까?",
-    finishDate: "2022-11-10",
-    active: true,
-  },
-]
+import DebateCard from "../DebateCard";
+import { HotTopicDebate } from "src/mock/DebateMockData";
 
 function Arena() {
   const calculateDday = (target: string) => {
-    let targetDate = new Date(`${target}T00:00:00+0900`)
-    let nowDate = new Date()
-    let diff = +targetDate - +nowDate
+    let targetDate = new Date(`${target}T00:00:00+0900`);
+    let nowDate = new Date();
+    let diff = +targetDate - +nowDate;
 
-    const diffDay = Math.floor(diff / (1000 * 60 * 60 * 24))
-    const diffHour = Math.floor((diff / (1000 * 60 * 60)) % 24)
-    const diffMin = Math.floor((diff / (1000 * 60)) % 60)
-    return `D-${Math.ceil((diffDay * 24 + diffHour + diffMin / 60) / 24)}`
-  }
+    const diffDay = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const diffHour = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const diffMin = Math.floor((diff / (1000 * 60)) % 60);
+    return `D-${Math.ceil((diffDay * 24 + diffHour + diffMin / 60) / 24)}`;
+  };
 
   return (
     <Container>
-      {DummyTopic.map((value, idx) => (
+      {HotTopicDebate.map((value, idx) => (
         <DebateCard key={idx} active={value.active}>
           <Link href={`/debate/${value.id}`}>
             <div>
@@ -52,7 +33,7 @@ function Arena() {
         </DebateCard>
       ))}
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
@@ -77,11 +58,11 @@ const Container = styled.div`
   &::-webkit-scrollbar {
     display: none;
   }
-`
+`;
 
 const Image = styled.img`
   margin: 0 auto 4px;
   width: 60px;
-`
+`;
 
-export default Arena
+export default Arena;
--- src/pages/debate/[id].tsx
+++ src/pages/debate/[id].tsx
@@ -1,23 +1,121 @@
-import React from "react"
-import styled from "@emotion/styled"
-import BullBearChart from "src/components/Debate/BullBearChart"
-import BullBearChart2 from "src/components/Debate/BullBearChart"
+import React from "react";
+import { useRouter } from "next/router";
+import styled from "@emotion/styled";
+
+import BullBearChart from "src/components/Debate/BullBearChart";
+import { HotTopicDebate } from "src/mock/DebateMockData";
+import { COLOR } from "src/styles/PALLETS";
+import CardList from "src/components/Home/CardList";
 
 function DebateDetail() {
+  const router = useRouter();
+  const query = router.query.id;
+  const mockData = HotTopicDebate.filter(
+    (value) => value.id === Number(query)
+  )[0];
+
   return (
     <Wrap>
-      <BullBearChart
-        bull={{ name: "10만전자 간다", vote: 156472 }}
-        bear={{ name: "4만전자 간다", vote: 162321 }}
-      />
+      <Top>
+        <h3 className="topic-title">{mockData?.title}</h3>
+        <div className="topic-asset">
+          <i></i>
+          {mockData?.relatedAsset}
+        </div>
+      </Top>
+      <Vote>
+        <p className="bullbear-header">
+          <span>Bull</span> | <span>Bear</span>
+        </p>
+        <BullBearChart
+          bull={{ name: mockData.bullTitle, vote: mockData.bull }}
+          bear={{ name: mockData.bearTitle, vote: mockData.bear }}
+        />
+        <SubTitle>Bull</SubTitle>
+        <CardList
+          type="article"
+          data={[
+            {
+              id: 1,
+              title: `아이폰 15 유출 떴다.jpg`,
+              board: "애플 팬클럽",
+            },
+            {
+              id: 12,
+              title: "재건축 승인 났다네요.jpg",
+              board: "서울부동산집값지키기 카페",
+            },
+            {
+              id: 13,
+              title: "카카오 이번에 ㄹㅇ 난리 났습니다",
+              board: "카카오 팬클럽",
+            },
+            {
+              id: 14,
+              title: "현재 몇 퍼센트인가요?",
+              board: "빅테크 주주들의 모임 카페",
+            },
+          ]}
+        />
+      </Vote>
     </Wrap>
-  )
+  );
 }
 const Wrap = styled.div`
   display: flex;
-  margin: 25px 0;
   flex-direction: column;
   gap: 25px;
-`
+`;
 
-export default DebateDetail
+const Top = styled.section`
+  margin: 32px 24px 0;
+  color: #fff;
+  .topic-title {
+    font-size: 1.3rem;
+    font-weight: 700;
+  }
+  .topic-asset {
+    display: flex;
+    align-items: center;
+    margin: 8px 0 0;
+    gap: 8px;
+    color: #000;
+    font-size: 0.8rem;
+    i {
+      display: block;
+      width: 16px;
+      height: 16px;
+      background-color: #fff;
+      border-radius: 100%;
+    }
+  }
+`;
+const Vote = styled.section`
+  margin: 0 -16px;
+  padding: 36px 4px 0;
+  min-height: 90vh;
+  background-color: #fff;
+  border-radius: 24px 24px 0px 0px;
+  .bullbear-header {
+    margin: 0 0 16px;
+    color: #9e9e9e;
+    font-size: 1.38rem;
+    font-weight: 700;
+    text-align: center;
+    span {
+      margin: 0 8px;
+    }
+    span:first-of-type {
+      color: ${COLOR.bull};
+    }
+    span:last-of-type {
+      color: ${COLOR.bear};
+    }
+  }
+`;
+const SubTitle = styled.h2`
+  font-size: 1.37rem;
+  font-weight: 600;
+`;
+
+export default DebateDetail;
